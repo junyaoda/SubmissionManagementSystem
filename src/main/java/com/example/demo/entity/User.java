0961f89f9d88b964c41bef67fc0431fa1db088d1
junya.oda <tyoudai59@gmail.com>
2024-04-08 23:11:00 +0900

    ファイル更新

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -4,7 +4,33 @@ import lombok.Data;
 
 @Data
 public class User {
-    int id;
-    String password;
-    String name;
+
+	int id;
+	String slackId;
+	String userName;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getSlackId() {
+		return slackId;
+	}
+
+	public void setSlackId(String slackId) {
+		this.slackId = slackId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
 }
